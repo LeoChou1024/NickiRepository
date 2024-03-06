d984caa65ffb00ba1286923b10f56257db614ebe
DESKTOP-K8B02EC\USER <nicki831028@gmail.com>
2024-03-06 12:41:11 +0800

    註冊帳號-登入更新

---
+++ GitProject/Login.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitProject
+{
+    internal class Login
+    {
+        public string Name { get; set; }
+        public int Password { get; set; }
+        public bool Loginfun(string name, int password) 
+        {
+            this.Name = name;
+            this.Password = password;
+
+            return true;
+
+        }
+    }
+}
