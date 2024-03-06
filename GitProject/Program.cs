ca05accc7ef7e89c44bc4f42bd071e615a74764f
DESKTOP-K8B02EC\USER <nicki831028@gmail.com>
2024-03-06 12:55:55 +0800

    註冊帳號-實作

--- GitProject/Program.cs
+++ GitProject/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using GitProject;
+
 Console.WriteLine("Hello, World!");
+
+Account account = new Account("Nicki", 1028);
+Console.WriteLine(account.Name);
+Console.WriteLine(account.Password);
+
+
+Console.ReadKey();
